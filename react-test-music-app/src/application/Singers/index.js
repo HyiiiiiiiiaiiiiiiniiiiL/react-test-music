aefd507fedaf1bf00c3134a88f69fbd45e8e524a
HiiYiiLii <HiiYiiLii@gitee.com>
2020-10-12 15:39:40 +0800

    歌手页数据

--- react-test-music-app/src/application/Singers/index.js
+++ react-test-music-app/src/application/Singers/index.js
@@ -1,23 +1,124 @@
 //cSpell:word horizen
-import React ,{useState}from 'react';
+import React ,{useState,useRef,useEffect}from 'react';
 import Horizen from '../../baseUI/horizen-item'
 import {categoryTypes, alphaTypes} from '../../api/config'
 import {NavContainer} from './style'
-function Singers() {
+import Scroll  from "../../baseUI/scroll/index"
+import {List,ListItem,ListContainer} from  './style.js'
+import {
+  getSingerList,
+  getHotSingerList,
+  changeEnterLoading,
+  changePageCount,
+  refreshMoreSingerList,
+  changePullUpLoading,
+  changePullDownLoading,
+  refreshMoreHotSingerList
+} from './store/actionCreators';
+import {connect} from 'react-redux'
+import { forceCheck } from 'react-lazyload';
+import Loading from '../../baseUI/loading'
+function Singers(props) {
   let [category,setCategory] = useState('')
   let [alpha,setAlpha] = useState('')
+  const listRef = useRef(null)
+
+  const {singerList,enterLoading,pullUpLoading,pullDownLoading,pageCount} = props
+
+const {getHotSingerDispatch,updateDispatch,pullDownRefreshDispatch,pullUpRefreshDispatch} = props
+
+useEffect(()=>{
+  getHotSingerDispatch()
+},[getHotSingerDispatch])
+
+const renderSingerList=()=>{
+  const list = singerList ? singerList.toJS(): [];
+return(
+  <div ref={listRef}>
+  <List>
+    {
+        list.map ((item, index) => {
+          return (
+            <ListItem key={item.accountId+""+index}>
+              <div className="img_wrapper">
+                <img src={`${item.picUrl}?param=300x300`} width="100%" height="100%" alt="music"/>
+              </div>
+              <span className="name">{item.name}</span>
+            </ListItem>
+          )
+        })
+      }
+  </List>
+  </div>
+)
+
+}
+
+
   let handleUpdateAlpha=(val)=>{
     setAlpha(val)
+    updateDispatch(category,val)
   }
   let handleUpdateCategory = (val)=>{
     setCategory(val)
+    updateDispatch(category,alpha)
   }
+  const handlePullUp = () => {
+    pullUpRefreshDispatch(category, alpha, category === '', pageCount);
+  };
+
+  const handlePullDown = () => {
+    pullDownRefreshDispatch(category, alpha);
+  };
   return  (
     <NavContainer>
     <Horizen list={categoryTypes} title={"分类 (默认热门):"} handleClick={(val)=>handleUpdateCategory(val)} oldVal={category}></Horizen>
     <Horizen list={alphaTypes} title={"首字母:"} handleClick={val=>handleUpdateAlpha(val)} oldVal={alpha}></Horizen>
+    <ListContainer>
+      <Scroll pullUp={handlePullUp} pullDown={handlePullDown} pullUpLoading={pullUpLoading} pullDownLoading={pullDownLoading} onScroll={forceCheck}>
+        {renderSingerList()}
+      </Scroll>
+      <Loading show={enterLoading}></Loading>
+    </ListContainer>
     </NavContainer>
   )
 }
 
-export default React.memo(Singers);
+
+const mapStateToProps=(state)=>({
+  singerList:state.getIn(['singers','singerList']),
+  enterLoading:state.getIn(['singers','enterLoading']),
+  pullUpLoading:state.getIn(['singers','pullUpLoading']),
+  pullDownLoading:state.getIn(['singers','pullDownLoading']),
+  pageCount:state.getIn(['singers','pageCount'])
+})
+const mapDispatchToProps=(dispatch)=>{
+  return {
+    getHotSingerDispatch(){
+      dispatch(getHotSingerList())
+    },
+    updateDispatch(category,alpha){
+      dispatch(changePageCount(0));
+      dispatch(getSingerList(alpha));
+    },
+    pullUpRefreshDispatch(category,alpha,hot,count){
+      dispatch(changePullUpLoading(true))
+      dispatch(changePageCount(count+1))
+      if(hot){
+          dispatch(refreshMoreHotSingerList())
+      }else{
+         dispatch(refreshMoreSingerList(alpha))
+      }
+    },
+    pullDownRefreshDispatch(category,alpha){
+      dispatch(changePullDownLoading(true))
+      dispatch(changePageCount(0))
+      if(category ==='' && alpha===''){
+        dispatch(getHotSingerList())
+      }else{
+        dispatch(getSingerList(alpha))
+      }
+    }
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Singers);
--- react-test-music-app/src/baseUI/loading/index.js
+++ react-test-music-app/src/baseUI/loading/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 import style from '../../assets/global-style';
+import {PropTypes} from 'prop-types'
 
 const loading = keyframes`
   0%, 100% {
@@ -13,8 +14,8 @@ const loading = keyframes`
 const LoadingWrapper = styled.div`
   >div {
     position: fixed;
-    left: 0; 
-    right: 0;  
+    left: 0;
+    right: 0;
     top: 0;
     bottom: 0;
     margin: auto;
@@ -30,13 +31,20 @@ const LoadingWrapper = styled.div`
   }
 `
 
-function Loading()  {
+function Loading(props)  {
+  const {show} = props
   return (
-    <LoadingWrapper>
+    <LoadingWrapper style={show?{display:""}:{display:"none"}}>
       <div></div>
       <div></div>
     </LoadingWrapper>
   );
 }
- 
+Loading.defaultProps={
+  show:true
+}
+Loading.propTypes={
+  show:PropTypes.bool
+}
+
 export default React.memo(Loading);

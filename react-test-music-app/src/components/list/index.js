6fa1fb665b4dfe85ebdb4c7281b0eed7adf20545
HiiYiiLii <HiiYiiLii@gitee.com>
2020-10-14 18:28:05 +0800

    动画切换效果

--- react-test-music-app/src/components/list/index.js
+++ react-test-music-app/src/components/list/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { 
+import {
   ListWrapper,
   ListItem,
   List
 } from './style';
 import LazyLoad from "react-lazyload";
 import { getCount } from "../../api/utils";
+import {withRouter} from 'react-router-dom'
+
 
 function RecommendList(props) {
+
+  const enterDetail = (id)=>{
+    props.history.push(`/recommend/${id}`)
+  }
   return (
     <ListWrapper>
       <h1 className="title">推荐歌单</h1>
@@ -15,7 +21,7 @@ function RecommendList(props) {
         {
           props.recommendList.map(item => {
             return (
-              <ListItem key={item.id}>
+              <ListItem key={item.id} onClick={()=>enterDetail(item.id)}>
                 <div className="img_wrapper">
                   <div className="decorate"></div>
                   <LazyLoad placeholder={<img width="100%" height="100%" src={require('./music.png')} alt="music"/>}>
@@ -27,13 +33,16 @@ function RecommendList(props) {
                   </div>
                 </div>
                 <div className="desc">{item.name}</div>
+
               </ListItem>
+
             )
           })
         }
       </List>
+
     </ListWrapper>
   );
   }
- 
-export default React.memo(RecommendList);
+
+export default React.memo(withRouter(RecommendList));
